93955204abb54ca5da192d3a11a0e9abe4cb5f77
Danny Hatoum <dannyhatoum@icloud.com>
2025-10-02 23:11:27 +0000

    Add all the files

---
+++ scripts/code.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import argparse
+import os
+
+main_path = "/home/dhatoum/projects/Assignments/A5/"
+
+df = pd.read_csv(main_path + "data/311_Service_Requests_from_2010_to_Present.csv")
